a9c43a091dcd318e9a8c7ee33ddec40c86d505eb
jnosek <jnosek@gmail.com>
2025-10-30 12:01:30 -0400

    added colorization to output removed large bottom margin

--- src/components/htmlLogger.ts
+++ src/components/htmlLogger.ts
@@ -6,8 +6,37 @@ class HTMLLogger {
     }
  
     static log(message: string): void {
+        this.logWithClass(message, "text-body");
+    }
+
+    static primary(message: string): void {
+        this.logWithClass(message, "text-primary");
+    }
+
+    static secondary(message: string): void {
+        this.logWithClass(message, "text-secondary");
+    }
+
+    static success(message: string): void {
+        this.logWithClass(message, "text-success");
+    }
+
+    static danger(message: string): void {
+        this.logWithClass(message, "text-danger");
+    }
+
+    static warning(message: string): void {
+        this.logWithClass(message, "text-warning");
+    }
+
+    static info(message: string): void {
+        this.logWithClass(message, "text-info");
+    }
+
+    private static logWithClass(message: string, className: string): void {
         if (this.consoleOutput) {
             const logEntry = document.createElement("pre");
+            logEntry.className = className;
             logEntry.textContent = message;
             this.consoleOutput.appendChild(logEntry);
         }
